2d402dfb097e1af8a32f2aa7b79c8a982f55e989
nikofiko <nikodemwilk69@gmail.com>
2025-05-07 20:33:15 +0200

    fix

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-import { useState } from 'react'
-
-
-const AccordionItem = ({name, description}) => {
-  const [visibility, setVisibility] = useState(false)
+const AccordionItem = ({ name, description, isOpen, onClick }) => {
   return (
-    <div className='flex flex-col hover:text-softred' onClick={() => setVisibility(prevVisibility => !prevVisibility)}>
+    <div className='flex flex-col'>
       <hr className='border-0.1 border-gray-300' />
-      <div className='cursor-pointer flex justify-between pt-[19px] pb-[13px]'>
-        <h4 className={`hover:text-softred text-[15px] lg:text-[18px] text-darkblue font-rubik ${visibility ? "" : "text-darkblue"}`}>{name}</h4>
-        <div className={`text-red-800 flex items-center ${visibility ? "rotate-180 " : "text-softblue"}`}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12"><path fill="none" stroke="currentColor" stroke-width="3" d="M1 1l8 8 8-8"/></svg>
+      <div 
+        className='cursor-pointer flex justify-between pt-[19px] pb-[13px] hover:text-softred transition-colors'
+        onClick={onClick}
+      >
+        <h4 className={`text-[15px] lg:text-[18px] font-rubik ${isOpen ? 'text-softred font-medium' : 'text-darkblue'}`}>
+          {name}
+        </h4>
+        <div className={`transition-transform duration-300 ${isOpen ? 'rotate-180 text-softred' : 'text-softblue'}`}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
+            <path fill="none" stroke="currentColor" strokeWidth="3" d="M1 1l8 8 8-8" />
+          </svg>
         </div>
       </div>
-      {visibility && 
-            (<p className='pb-[29px] leading-[36px] text-[14px] lg:text-[16px] text-grayish font-rubik'>{description}</p>)
-        }
-      
+      {isOpen && (
+        <p className='pb-[29px] leading-[36px] text-[14px] lg:text-[16px] text-grayish font-rubik'>
+          {description}
+        </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default AccordionItem
+export default AccordionItem;
